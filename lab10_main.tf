1853f1bdb20f09a32cb24f5584c05c46ecfa4290
pradeepgithublab <pr.bhilare@gmail.com>
2025-03-20 17:49:57 +0530

    Add files via upload

---
+++ lab10_main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+############ Creating FIFO Queue ############
+resource "aws_sqs_queue" "queue" {
+  name = "MyWhizQueue.fifo"
+  fifo_queue = true
+  content_based_deduplication = true
+}
+############ Creating Standard Queue ############
+resource "aws_sqs_queue" "queue2" {
+  name = "MyWhizQueue"
+}
